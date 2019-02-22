8521f599a7db041a4171656e4c201550faafa6e4
jzamoral <jzamoral@poder-judicial.go.cr>
2019-02-22 11:18:56 -0600

    TODO EN ADMIN

--- PJInventario/Controllers/AdminController.cs
+++ PJInventario/Controllers/AdminController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PJInventario.Models;
+using System.Data.Entity;
 
 namespace PJInventario.Controllers
 {
     public class AdminController : Controller
     {
+        PJInventarioEntities db = new PJInventarioEntities();
+        
         // GET: Admin
         public ActionResult AdminIndex()
         {
             return View();
+        }
+        // GET: Despacho
+        public ActionResult DespachoIndex()
+        {
+            var despacho = db.Despacho.Include(d => d.Circuito);
+            return View(despacho.ToList());
         }
     }
 }
