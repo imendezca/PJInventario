1ed5efa23da1e914e27873e7e2f1bf71f868e6f8
Santiago Méndez <imendezca@poder-judicial.go.cr>
2019-02-22 15:15:16 -0600

    Tipo equipo index y tipo equipo data nuevos

---
+++ PJInventario/Data/TipoEquipoData.cs
@@ -0,0 +1,19 @@
+﻿using PJInventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PJInventario.Data
+{
+    public class TipoEquipoData
+    {
+        static PJInventarioEntities db = new PJInventarioEntities();
+
+        public static List<TipoEquipo> ListTipoEquipo()
+        {
+            var listaTipoEquipo= db.TipoEquipo;
+            return listaTipoEquipo.ToList();
+        }
+    }
+}
